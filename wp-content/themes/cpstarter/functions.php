a591f1046374da60a931166ecb637cef04e549e1
Student <student@CVS-R213D009.nic.bc.ca>
2017-12-06 13:31:06 -0800

    more chapter 6 changes, 5-7 videos

--- wp-content/themes/cpstarter/functions.php
+++ wp-content/themes/cpstarter/functions.php
@@ -131,6 +131,8 @@ function cpstarter_scripts() {
 		'collapse' => __( 'Collapse child menu', 'cpstarter'),
 	));
 
+	wp_enqueue_script( 'cpstarter-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20171206', true );
+	
 	wp_enqueue_script( 'cpstarter-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
