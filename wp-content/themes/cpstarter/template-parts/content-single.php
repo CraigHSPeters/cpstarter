7755e775096e9731bc7e8e2de1bf8f069c71c5fe
Student <student@CVS-R212D016.nic.bc.ca>
2017-12-09 14:40:28 -0800

    chapter 5 and 6 changes

--- wp-content/themes/cpstarter/template-parts/content-single.php
+++ wp-content/themes/cpstarter/template-parts/content-single.php
@@ -12,7 +12,6 @@
 <article class="hentry" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 	<?php cpstarter_the_category_list(); ?>
-
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
